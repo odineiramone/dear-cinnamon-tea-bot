4bc44ea3754fe0ea70becefa14500a830004450c
Odinei Ribeiro <odinei.ribeiro@plataformatec.com.br>
2018-10-25 00:40:22 -0300

    Change forecasting endpoint (#5)
    Using daily API to get forecast for the current day

--- handlers/weather.rb
+++ handlers/weather.rb
@@ -1,27 +1,36 @@
+require 'pry'
+
 module Lita
   module Handlers
     class Weather < Handler
       CLIMATEMPO_API_TOKEN = ENV['CLIMATEMPO_API_TOKEN']
       CLIMATEMPO_CITY_CODE = ENV['CLIMATEMPO_CITY_CODE']
-      HOURS_PER_DAY = 24
 
       route(/weather please/, :weather, command: true,
         help: { weather: 'replies with current weather for Sao Paulo, Brazil' })
 
       def weather(response)
-        temperatures_for_today = http.get("http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/#{CLIMATEMPO_CITY_CODE}/hours/72?token=#{CLIMATEMPO_API_TOKEN}")
-                                     .yield_self { |http_response| MultiJson.load(http_response.body) }
-                                     .yield_self { |forecasts| forecasts.fetch('data').first(HOURS_PER_DAY) }
-                                     .map        { |forecasts_for_today| forecasts_for_today.dig('temperature', 'temperature') }
-
-        min, max = temperatures_for_today.minmax
-
         temperature, sensation = http.get("http://apiadvisor.climatempo.com.br/api/v1/weather/locale/#{CLIMATEMPO_CITY_CODE}/current?token=#{CLIMATEMPO_API_TOKEN}")
                                      .yield_self { |http_response| MultiJson.load(http_response.body) }
                                      .yield_self { |weather| [weather.dig("data", "temperature"), weather.dig("data", "temperature")]  }
 
         response.reply("Está fazendo #{temperature}˚C com aquela sensação térmica de #{sensation}˚C")
-        response.reply("Hoje está previsto mínima de #{min}˚C e máxima de #{max}˚C 😄")
+
+        forecasts_for_today = http.get("http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/#{CLIMATEMPO_CITY_CODE}/days/15?token=#{CLIMATEMPO_API_TOKEN}")
+                                  .yield_self { |http_response| MultiJson.load(http_response.body) }
+                                  .yield_self { |forecasts| forecasts.fetch('data').first }
+
+        rain_probability = forecasts_for_today.dig('rain', 'probability')
+        temperature_max  = forecasts_for_today.dig('temperature', 'max')
+        temperature_min  = forecasts_for_today.dig('temperature', 'min')
+        phrase           = forecasts_for_today.dig('text_icon', 'text', 'phrase', 'reduced')
+
+        text = <<~FORECAST_TEXT
+          #{phrase}
+          Temperaturas variando entre mínima de #{temperature_min}˚C e máxima de #{temperature_max}˚C e probabilidade de chuva em #{rain_probability}% 😄
+        FORECAST_TEXT
+
+        response.reply(text)
       end
 
       Lita.register_handler(self)
