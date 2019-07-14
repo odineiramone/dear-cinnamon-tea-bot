08c40e7d09ba87d8a59fb52fdd46d35b5367a3f8
Odinei Ramone <odineiramone@gmail.com>
2019-07-13 21:44:13 -0300

    Refactored Weather handler to use services

--- handlers/weather.rb
+++ handlers/weather.rb
@@ -8,33 +8,35 @@ module Lita
         help: { weather: 'replies with current weather for Sao Paulo, Brazil' })
 
       def weather(response)
-        temperature, sensation = http.get("http://apiadvisor.climatempo.com.br/api/v1/weather/locale/#{CLIMATEMPO_CITY_CODE}/current?token=#{CLIMATEMPO_API_TOKEN}")
-                                     .yield_self { |http_response| MultiJson.load(http_response.body) }
-                                     .yield_self { |weather| [weather.dig("data", "temperature"), weather.dig("data", "temperature")]  }
-
-        forecasts_for_today = http.get("http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/#{CLIMATEMPO_CITY_CODE}/days/15?token=#{CLIMATEMPO_API_TOKEN}")
-                                  .yield_self { |http_response| MultiJson.load(http_response.body) }
-                                  .yield_self { |forecasts| forecasts.fetch('data').first }
-
-        today            = forecasts_for_today.fetch('date_br')
-        rain_probability = forecasts_for_today.dig('rain', 'probability')
-        temperature_max  = forecasts_for_today.dig('temperature', 'max')
-        temperature_min  = forecasts_for_today.dig('temperature', 'min')
-
-        title                 = "Previsão do tempo para #{today}"
-        weather_text          = "Está fazendo #{temperature}˚C com sensação térmica de #{sensation}˚C"
-        summary_text          = forecasts_for_today.dig('text_icon', 'text', 'phrase', 'reduced')
-        temperature_text      = "Temperaturas variando entre mínima de #{temperature_min}˚C e máxima de #{temperature_max}˚C"
-        rain_probability_text = "Probabilidade de chuva em #{rain_probability}% 😄"
+        @weather = WeatherService.execute
+        @forecast = ForecastService.execute
 
         response.reply(title)
         response.reply(weather_text)
-        response.reply(summary_text)
+        response.reply(@forecast.summary)
         response.reply(temperature_text)
         response.reply(rain_probability_text)
       end
 
       Lita.register_handler(self)
+
+      private
+
+      def title
+        "Previsão do tempo para #{@forecast.today}"
+      end
+
+      def weather_text
+        "Está fazendo #{@weather.temperature}˚C com sensação térmica de #{@weather.sensation}˚C"
+      end
+
+      def temperature_text
+        "Temperaturas variando entre mínima de #{@forecast.temperature_min}C e máxima de #{@forecast.temperature_max}C"
+      end
+
+      def rain_probability_text
+        "Probabilidade de chuva em #{@forecast.rain_probability} 😄"
+      end
     end
   end
 end
